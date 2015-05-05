591549e08f44ff3a552d8430faaf319e58016329
Mads Ravn <madsravn@gmail.com>
2015-05-05 16:59:51 +0200

    tests4 tirsdag

--- code/tests4/with_jumps/kdtree_vs_ort_graphs_simple.sh
+++ code/tests4/with_jumps/kdtree_vs_ort_graphs_simple.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-gnuplot -e "filename='hori_17.dat'; xtitle='Size of window'; ytitle='Average time in microseconds'; toptitle='KDTree vs Ort - horizontal 17'; name1='Ort'; name2='KDTree'" plot123.pg > hori_17.png
+UPTO=$1
+let UPTO=UPTO+1
+COUNTER=17
 
-gnuplot -e "filename='hori_18.dat'; xtitle='Size of window'; ytitle='Average time in microseconds'; toptitle='KDTree vs Ort - horizontal 18'; name1='Ort'; name2='KDTree'" plot123.pg > hori_18.png
+while [ $COUNTER -lt $UPTO ]; do
 
-gnuplot -e "filename='vert_17.dat'; xtitle='Size of window'; ytitle='Average time in microseconds'; toptitle='KDTree vs Ort - vertical 17'; name1='Ort'; name2='KDTree'" plot123.pg > vert_17.png
+    gnuplot -e "filename='hori_$COUNTER.dat'; xtitle='Size of window'; ytitle='Average time in microseconds'; toptitle='KDTree vs Ort - horizontal $COUNTER'; name1='Ort'; name2='KDTree'" plot123.pg > hori_$COUNTER.png
 
-gnuplot -e "filename='vert_18.dat'; xtitle='Size of window'; ytitle='Average time in microseconds'; toptitle='KDTree vs Ort - vertical 18'; name1='Ort'; name2='KDTree'" plot123.pg > vert_18.png
+    gnuplot -e "filename='vert_$COUNTER.dat'; xtitle='Size of window'; ytitle='Average time in microseconds'; toptitle='KDTree vs Ort - vertical $COUNTER'; name1='Ort'; name2='KDTree'" plot123.pg > vert_$COUNTER.png
 
-gnuplot -e "filename='hori_17.dat'; xtitle='Size of window'; ytitle='Jumps'; toptitle='horizontal 17'; name1='Average jumps'; name2='Maximum jump'" plot145.pg > jump_hori_17.png
+    gnuplot -e "filename='hori_$COUNTER.dat'; xtitle='Size of window'; ytitle='Jumps'; toptitle='horizontal $COUNTER'; name1='Average jumps'; name2='Maximum jump'" plot145.pg > jump_hori_$COUNTER.png
 
+    gnuplot -e "filename='vert_$COUNTER.dat'; xtitle='Size of window'; ytitle='Jumps'; toptitle='vertical $COUNTER'; name1='Average jumps'; name2='Maximum jump'" plot145.pg > jump_vert_$COUNTER.png
 
-gnuplot -e "filename='hori_18.dat'; xtitle='Size of window'; ytitle='Jumps'; toptitle='horizontal 18'; name1='Average jumps'; name2='Maximum jump'" plot145.pg > jump_hori_18.png
+    gnuplot -e "filename='hori_$COUNTER.dat'; xtitle='Size of window'; ytitle='Level'; toptitle='horizontal $COUNTER'; name1='Average level'; name2='Maximum level'; name3='Minimum level'; name4='amount of tens'" plot16789.pg > level_hori_$COUNTER.png
 
-gnuplot -e "filename='vert_17.dat'; xtitle='Size of window'; ytitle='Jumps'; toptitle='vertical 17'; name1='Average jumps'; name2='Maximum jump'" plot145.pg > jump_vert_17.png
-
-gnuplot -e "filename='vert_18.dat'; xtitle='Size of window'; ytitle='Jumps'; toptitle='vertical 18'; name1='Average jumps'; name2='Maximum jump'" plot145.pg > jump_vert_18.png
-
-gnuplot -e "filename='hori_17.dat'; xtitle='Size of window'; ytitle='Level'; toptitle='horizontal 17'; name1='Average level'; name2='Maximum level'; name3='Minimum level'; name4='amount of tens'" plot16789.pg > level_hori_17.png
-
-gnuplot -e "filename='hori_18.dat'; xtitle='Size of window'; ytitle='Level'; toptitle='horizontal 18'; name1='Average level'; name2='Maximum level'; name3='Minimum level'; name4='amount of tens'" plot16789.pg > level_hori_18.png
-
-gnuplot -e "filename='vert_17.dat'; xtitle='Size of window'; ytitle='Level'; toptitle='vertical 17'; name1='Average level'; name2='Maximum level'; name3='Minimum level'; name4='amount of tens'" plot16789.pg > level_vert_17.png
-
-gnuplot -e "filename='vert_18.dat'; xtitle='Size of window'; ytitle='Level'; toptitle='vertical 18'; name1='Average level'; name2='Maximum level'; name3='Minimum level'; name4='amount of tens'" plot16789.pg > level_vert_18.png
+    gnuplot -e "filename='vert_$COUNTER.dat'; xtitle='Size of window'; ytitle='Level'; toptitle='vertical $COUNTER'; name1='Average level'; name2='Maximum level'; name3='Minimum level'; name4='amount of tens'" plot16789.pg > level_vert_$COUNTER.png
+    
+    let COUNTER=COUNTER+1
 
 
+
+done
