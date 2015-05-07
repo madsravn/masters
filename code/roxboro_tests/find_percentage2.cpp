ca685284397504b7dbf4fe605bfb1daca1e95682
Mads Ravn <madsravn@gmail.com>
2015-05-07 22:56:43 +0200

    roxboro tests

--- code/roxboro_tests/find_percentage2.cpp
+++ code/roxboro_tests/find_percentage2.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <cmath>
 
 
 
@@ -27,39 +28,42 @@ int main(int argc, char** argv) {
    
     if(argc == 3) {
         std::ifstream myfile(argv[1]);
-        std::string name(argv[2]);
         std::string line;
+        int lgn = 0;
         int current = 0;
         int placement = 0;
-        int ort_avg;
-        int max_jump;
-        float avg_jump;
-        float level_avg;
-        int level_max;
-        int level_min;
-        int kd_avg;
-        int size;
-        int lastsize = 0;
-        std::ofstream output;
+        int percentage = std::stoi(argv[2]);
+        float diff = 1000000;
+        int x = 0;
+        int kdtree;
+        int ort;
+        int windowsize = 0;
+        int maxjump = 0;
+        float avg_jump = 0;
+        float jumps_per_result = 0;
+        int out_maxjump = 0;
+        float out_avg_jump = 0;
+        float out_jumps_per_result = 0;
+
         if (myfile.is_open()) {
             while ( myfile.good()) {
                 std::getline (myfile,line);
                 if(line.size() > 0) {
                     auto tokens = split(line, ' ');
                     if(tokens.at(0) == "TESTING") {
-                        int temp = std::stoi(tokens.at(1));
-                        if(current != temp) {
-                            current = temp;
-                            lastsize = 0;
-                            if(output.is_open()) {
-                                output.flush();
-                                output.close();
-                            }
-                            output.open(name + "_" + std::to_string(current) + ".dat");
+                        int t = std::stoi(tokens.at(1));
+                        current = std::stoi(tokens.at(3));
+                        if(lgn < t && x == 1) {
+                            std::cout << lgn << " " << windowsize << " " << out_maxjump << " " << out_avg_jump << " " << out_jumps_per_result << " " << std::sqrt(std::pow(2, lgn)) << std::endl;
+
+                            lgn = t;
+                            diff = 1000000;
+                        } else {
+                            lgn = t;
                         }
+                        x = 1;
                         if(tokens.at(5) == "(ORT)") {
                             placement = 0;
-                            size = std::stoi(tokens.at(3));
                         }
                         if(tokens.at(5) == "(JUMPS)") {
                             placement = 1;
@@ -67,35 +71,39 @@ int main(int argc, char** argv) {
                         if(tokens.at(5) == "(KDTREE)") {
                             placement = 2;
                         }
-                    }
-                    if(tokens.at(0) == "Average") {
-                        if(placement == 0) {
-                            ort_avg = std::stoi(tokens.at(2));
-                        }
-                        if(placement == 2) {
-                            kd_avg = std::stoi(tokens.at(2));
-                            if(lastsize < size) {
-                                lastsize = size;
-                                output << size << " " << ort_avg << " " << kd_avg << " " << avg_jump << " " << max_jump << " " << level_avg << " " << level_max << " " << level_min  << std::endl;
+                    } else {
+                        if(tokens.at(0) == "Average") {
+                            if(placement == 0) {
+                                ort = std::stoi(tokens.at(2));
+                            }
+                            if(placement == 2) {
+                                kdtree = std::stoi(tokens.at(2));
+                                if(fabs(percentage - 100*float(kdtree)/float(ort)) < diff) {
+                                    diff = fabs(percentage - 100*float(kdtree)/float(ort));
+                                    windowsize = current;
+                                    out_maxjump = maxjump;
+                                    out_avg_jump = avg_jump;
+                                    out_jumps_per_result = jumps_per_result;
+                                }
+ 
+
                             }
                         }
-                    }
-                    if(tokens.at(0) == "That") {
-                        avg_jump = std::stof(tokens.at(2));
-                    }
-                    if(tokens.at(0) == "With") {
-                        max_jump = std::stoi(tokens.at(5));
-                    }
-                    if(tokens.at(0) == "startlevel") {
-                        level_avg = std::stof(tokens.at(8));
-                        level_max = std::stoi(tokens.at(3));
-                        level_min = std::stoi(tokens.at(13));
+                        if(tokens.at(0) == "That") {
+                            jumps_per_result = std::stof(tokens.at(2));
+                        }
+                        if(tokens.at(0) == "With") {
+                            maxjump = std::stoi(tokens.at(5));
+                            avg_jump = std::stof(tokens.at(10));
+                        }
                     }
 
                 }
             }
             myfile.close();
         }
+
+        std::cout << lgn << " " << windowsize << " " << out_maxjump << " " << out_avg_jump << " " << out_jumps_per_result << " " << std::sqrt(std::pow(2,lgn)) <<  std::endl;
     }
     return 0;
 
