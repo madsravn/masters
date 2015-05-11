9f8a705e1d52f54aa151f474695ab9e682db26d1
Mads Ravn <madsravn@gmail.com>
2015-05-11 12:29:10 +0200

    main til roxboro

--- code/main.cpp
+++ code/main.cpp
@@ -311,7 +311,7 @@ void measureTimeOfPackedInts() {
 
 void TESTS() {
     
-    Tester test(25);
+    Tester test(23);
 
     //test.how_much_faster_is_ort_vertical("Testing how much faster Ort is than KDTree vertical");
     test.run(2,0);
